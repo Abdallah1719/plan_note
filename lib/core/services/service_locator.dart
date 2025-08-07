0970dbf13c2bddf17da3000a242bdafd37bd0a1a
abdallah <abdallahtolba17@gmail.com>
2025-08-07 11:50:23 +0300

    Initialized service locator & shared prefereneces

--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -1,12 +1,14 @@
 import 'package:get_it/get_it.dart';
 import 'package:plan_note/core/storage/shared_preferences/cache_helper.dart';
 import 'package:plan_note/core/theme/cubit/theme_cubit.dart';
+import 'package:plan_note/l10n/cubit/locale_cubit.dart';
 
 final sl = GetIt.instance;
 void setupServiceLocator() {
-   //==================== shared preferences ====================
+  //==================== shared preferences ====================
   sl.registerSingleton<CacheHelper>(CacheHelper());
   //==================== theme cubit ====================
-   sl.registerFactory<ThemeCubit>(() => ThemeCubit());
-
+  sl.registerFactory<ThemeCubit>(() => ThemeCubit());
+  //==================== locale cubit ====================
+  sl.registerFactory<LocaleCubit>(() => LocaleCubit());
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:plan_note/core/services/service_locator.dart';
+import 'package:plan_note/core/storage/shared_preferences/cache_helper.dart';
 import 'package:plan_note/plan_note_app.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  setupServiceLocator();
+  await CacheHelper().init();
   runApp(const PlanNoteApp());
 }
