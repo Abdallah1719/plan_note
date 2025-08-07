c91284b1b3fbbafc4be0fd3877746de061dc8b47
abdallah <abdallahtolba17@gmail.com>
2025-08-07 11:29:34 +0300

    create shared preferences file cache helper

--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -1,4 +1,8 @@
 import 'package:get_it/get_it.dart';
+import 'package:plan_note/core/storage/shared_preferences/cache_helper.dart';
 
 final sl = GetIt.instance;
-void setupServiceLocator() {}
+void setupServiceLocator() {
+   //==================== shared preferences ====================
+  sl.registerSingleton<CacheHelper>(CacheHelper());
+}
