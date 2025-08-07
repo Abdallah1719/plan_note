2ca895cec5ced2325fa7a9fc41e19e8e0704da77
abdallah <abdallahtolba17@gmail.com>
2025-08-07 11:37:09 +0300

    create theme cubit

--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -1,8 +1,12 @@
 import 'package:get_it/get_it.dart';
 import 'package:plan_note/core/storage/shared_preferences/cache_helper.dart';
+import 'package:plan_note/core/theme/cubit/theme_cubit.dart';
 
 final sl = GetIt.instance;
 void setupServiceLocator() {
    //==================== shared preferences ====================
   sl.registerSingleton<CacheHelper>(CacheHelper());
+  //==================== theme cubit ====================
+   sl.registerFactory<ThemeCubit>(() => ThemeCubit());
+
 }
